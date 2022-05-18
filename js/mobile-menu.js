0f69c7802f95b196ac67409e9fb4f44c20be1b23
lubamos <98753340+lubamos@users.noreply.github.com>
2022-05-18 10:57:46 +0300

    menu-modal

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-menu-button]"),
-    closeModalBtn: document.querySelector("[data-menu-button]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-open");
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
